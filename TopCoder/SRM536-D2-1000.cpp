f35701fe986798dc300209e6871105902355249a
ilyesG <38229094+ilyesG@users.noreply.github.com>
2018-06-11 01:50:37 +0100

    Update SRM536-D2-1000.cpp

--- TopCoder/SRM536-D2-1000.cpp
+++ TopCoder/SRM536-D2-1000.cpp
@@ -9,7 +9,17 @@ const ld PI =  3.141592653589793238L;
 const ll N=100002;
 using namespace std;
 
-
+/**
+*My approach was dp+memoization.I sorted the array in increasing order and begin looping from the start.
+*At each index i: I loop from j=i+k-2 to j=n-1,
+*and each time I compute the result of merging the j-i+1(from i to j) companies with the rest which is one company; the result of merging all the remaining companies
+*i.e. companies from j+1 to n-1.Then I take the max and return it, also storing in the process the denominator.
+*This works when I sort the array in this order because of the observation, that for any order chosen for the array, the final look of it after the merge
+*will be in the form of consecutive groups of adjacent companies of size atleast k-1(last group must be of size >= k) divided by the product
+*of the number of companies in it multiplied by the number of companies in each previous group.So the denominator gets bigger with every group,
+*that's why the optimal answer will be in the case where the smallest numbers come in last, and the biggest in first.The only thing remaining
+*is to figure out how many companies in each group; which is the job of the recursive dp function(solve).
+*/
 
 class MergersDivTwo{
 	public:
@@ -29,9 +39,9 @@ class MergersDivTwo{
      if(ans!=-1e10) return ans;
 
      ans=(double)-1e9;
-     for(ll ii=i+k-2;ii<n;++ii){
-        ll pp=p*(ii-i+2-(ii==n-1));
-        ans=max(ans,(double)(sum(v,i,ii)/pp)+solve(v,n,ii+1,pp,k));
+     for(ll j=i+k-2;j<n;++j){
+        ll pp=p*(j-i+2-(j==n-1));
+        ans=max(ans,(double)(sum(v,i,j)/pp)+solve(v,n,j+1,pp,k));
      }
 
      return ans;
