206f1fd8736983b1944425df9c92c88f4aa8b33f
Mohamed Ilyes Ltifi <ilyes.glays@gmail.com>
2019-08-29 15:48:22 +0100

    Update SRM312-D1-500.cpp

--- TopCoder/SRM312-D1-500.cpp
+++ TopCoder/SRM312-D1-500.cpp
@@ -82,12 +82,3 @@ class AntarcticaPolice {
 		return sumVal / (LD)nbVal;
 	}
 };
-
-signed main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-
-	AntarcticaPolice x;
-	cout << x.minAverageCost({5, 6, 7, 8}, {"NYNN", "YNNN", "NNNY", "NNYN"});
-}
