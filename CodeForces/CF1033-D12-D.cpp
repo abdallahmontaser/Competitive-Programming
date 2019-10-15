c5deba0ec1db966e49942e0dbaa70f91ee4dd83a
Mohamed Ilyes Ltifi <ilyes.glays@gmail.com>
2019-10-15 19:39:03 +0100

    Update CF1033-D12-D.cpp

--- CodeForces/CF1033-D12-D.cpp
+++ CodeForces/CF1033-D12-D.cpp
@@ -1,6 +1,3 @@
-/**
-  * Editorial: https://codeforces.com/blog/entry/62287
-*/
 #include <bits/stdc++.h>
 #define LL long long
 #define LD long double
@@ -11,69 +8,43 @@
 using namespace std; const LD EPS = 1e-9L;
 const int N = 500 + 5, LOGN = 20, inf = 1e9, mod = 998244353;
 /**************/
-vector<LL> a; map<LL, int> occ;
-int32_t main() {
+LL binSearch(int lo, int hi, LL x, function<LL(LL)> f) {
+      while (lo <= hi) {
+            LL mid = (lo + hi) >> 1;
+            LL cur = f(mid);
+
+            if (cur == x) return mid;
+            if (cur < x) lo = mid + 1;
+            else hi = mid - 1;
+      }
+      return 0LL;
+}
+vector<LL> a; map<LL, int> nbAi, nbPrime;
+int main() {
       ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
       int n; cin >> n;
-      for (int i = 1; i <= n; ++i) {
-            LL x; cin >> x;
-            ++occ[x];
-            if (occ[x] == 1) a.push_back(x);
-      }
+      for (int i = 1; i <= n; ++i) { LL x; cin >> x; ++nbAi[x]; if (nbAi[x] == 1) a.push_back(x); }
 
-      map<LL, int> primeOcc;
       int ans = 1;
       for (int i = 0; i < sz(a); ++i) {
-            int cnt = occ[a[i]];
-            LL p = 0;
-            LL lo = 1, hi = 6e4;
-            while (lo <= hi) {
-                  LL mid = (lo + hi) / 2;
-                  LL cur = mid * mid * mid * mid;
+            int cnt = nbAi[a[i]];
 
-                  if (cur == a[i]) p = mid;
-                  if (cur < a[i]) lo = mid + 1;
-                  else hi = mid - 1;
-            }
-            if (p) {
-                  primeOcc[p] += 4 * cnt; continue;
-            }
+            LL p = binSearch(1LL, (LL)6e4, a[i], [](LL arg) -> LL { return arg * arg * arg * arg; });
+            if (p) { nbPrime[p] += 4 * cnt; continue; }
 
-            lo = 1, hi = 2e6;
-            while (lo <= hi) {
-                  LL mid = (lo + hi) / 2;
-                  LL cur = mid * mid * mid;
+            p = binSearch(1LL, (LL)2e6, a[i], [](LL arg) -> LL { return arg * arg * arg; });
+            if (p) { nbPrime[p] += 3 * cnt; continue; }
 
-                  if (cur == a[i]) p = mid;
-                  if (cur < a[i]) lo = mid + 1;
-                  else hi = mid - 1;
-            }
-            if (p) {
-                  primeOcc[p] += 3 * cnt; continue;
-            }
-
-            lo = 1, hi = 2e9;
-            while (lo <= hi) {
-                  LL mid = (lo + hi) / 2;
-                  LL cur = mid * mid;
-
-                  if (cur == a[i]) p = mid;
-                  if (cur < a[i]) lo = mid + 1;
-                  else hi = mid - 1;
-            }
-            if (p) {
-                  primeOcc[p] += 2 * cnt; continue;
-            }
+            p = binSearch(1LL, (LL)2e9, a[i], [](LL arg) -> LL { return arg *  arg; });
+            if (p) { nbPrime[p] += 2 * cnt; continue; }
 
             for (int j = 0; j < sz(a); ++j) if (a[i] != a[j] && __gcd(a[i], a[j]) != 1) p = __gcd(a[i], a[j]);
-            if (p) {
-                  primeOcc[p] += cnt; primeOcc[a[i] / p] += cnt; continue;
-            }
+            if (p) { nbPrime[p] += cnt; nbPrime[a[i] / p] += cnt; continue; }
 
             ans = ((LL)ans * (cnt + 1)) % mod;
             ans = ((LL)ans * (cnt + 1)) % mod;
       }
 
-      for (auto el: primeOcc) ans = ((LL)ans * (el.S + 1)) % mod;
+      for (auto element: nbPrime) ans = ((LL)ans * (element.S + 1)) % mod;
       cout << ans << '\n';
 }
