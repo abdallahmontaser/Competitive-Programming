5abcc0167b46a7f36c0896dfb428fbfc76a6d5fd
Mohamed Ilyes Ltifi <ilyes.glays@gmail.com>
2019-10-15 21:28:23 +0100

    Update CF1033-D12-D.cpp

--- CodeForces/CF1033-D12-D.cpp
+++ CodeForces/CF1033-D12-D.cpp
@@ -7,7 +7,8 @@
 #define all(x) x.begin(), x.end()
 using namespace std; const LD EPS = 1e-9L;
 const int N = 500 + 5, LOGN = 20, inf = 1e9, mod = 998244353;
-/**************/
+/***********************************************************/
+#define int long long
 LL binSearch(int lo, int hi, LL x, function<LL(LL)> f) {
       while (lo <= hi) {
             LL mid = (lo + hi) >> 1;
@@ -20,31 +21,27 @@ LL binSearch(int lo, int hi, LL x, function<LL(LL)> f) {
       return 0LL;
 }
 vector<LL> a; map<LL, int> nbAi, nbPrime;
-int main() {
+int32_t main() {
       ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
       int n; cin >> n;
-      for (int i = 1; i <= n; ++i) { LL x; cin >> x; ++nbAi[x]; if (nbAi[x] == 1) a.push_back(x); }
+      for (int i = 1; i <= n; ++i) { LL x; cin >> x; ++nbAi[x]; a.push_back(x); }
+	sort(all(a)); a.resize(unique(all(a)) - begin(a));
 
       int ans = 1;
       for (int i = 0; i < sz(a); ++i) {
             int cnt = nbAi[a[i]];
 
-            LL p = binSearch(1LL, (LL)6e4, a[i], [](LL arg) -> LL { return arg * arg * arg * arg; });
-            if (p) { nbPrime[p] += 4 * cnt; continue; }
+            if (LL p = binSearch(1LL, (LL)6e4, a[i], [](LL arg) -> LL { return arg * arg * arg * arg; })) nbPrime[p] += 4 * cnt;
+            else if (LL p = binSearch(1LL, (LL)2e6, a[i], [](LL arg) -> LL { return arg * arg * arg; })) nbPrime[p] += 3 * cnt;
+            else if (LL p = binSearch(1LL, (LL)2e9, a[i], [](LL arg) -> LL { return arg * arg; })) nbPrime[p] += 2 * cnt;
+            else {
+                  LL g = 1;
+                  for (int j = 0; j < sz(a); ++j) if (i != j) g = max(g, __gcd(a[i], a[j]));
 
-            p = binSearch(1LL, (LL)2e6, a[i], [](LL arg) -> LL { return arg * arg * arg; });
-            if (p) { nbPrime[p] += 3 * cnt; continue; }
-
-            p = binSearch(1LL, (LL)2e9, a[i], [](LL arg) -> LL { return arg *  arg; });
-            if (p) { nbPrime[p] += 2 * cnt; continue; }
-
-            for (int j = 0; j < sz(a); ++j) if (a[i] != a[j] && __gcd(a[i], a[j]) != 1) p = __gcd(a[i], a[j]);
-            if (p) { nbPrime[p] += cnt; nbPrime[a[i] / p] += cnt; continue; }
-
-            ans = ((LL)ans * (cnt + 1)) % mod;
-            ans = ((LL)ans * (cnt + 1)) % mod;
+                  if (g != 1) nbPrime[g] += cnt, nbPrime[a[i] / g] += cnt;
+                  else ans = ((LL)ans * (cnt + 1) * (cnt + 1)) % mod;
+            }
       }
-
       for (auto element: nbPrime) ans = ((LL)ans * (element.S + 1)) % mod;
       cout << ans << '\n';
 }
